9c62685fc8c04314e9606b31e949b07ffc6b645a
colsipple <colsipple@gmail.com>
2018-08-30 11:00:13 -0500

    victor's start

---
+++ airdistance.c
@@ -0,0 +1,27 @@
+/**
+* Author: Victor Nguyen
+* Date: 08/29/2018
+*
+* This program will demonstrate the air distance project
+*
+*/
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<math.h>
+
+
+int main(int argc, char **argv[]) {
+  double lat, lon, lat2, lon2, r, d;
+  r = 6371;
+  
+  printf("Enter the lattitude: ");
+  scanf("%lf", &lat );
+
+  printf("Enter the longitude: ");
+  scanf("%lf", &lon );
+
+
+
+  return 0;
+}
